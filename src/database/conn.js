338a87c29aa91ebd326090971ff81f9a75ab5b15
Rajnish Tripathi <rajnish.tripathi@unthinkable.co>
2023-09-21 14:46:24 +0530

    env change

--- src/database/conn.js
+++ src/database/conn.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose")
 const DATABASE_URL = process.env.DATABASE_URL;
 
 
-mongoose.connect(DATABASE_URL)
+mongoose.connect(DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
     .then(() => console.log("database connected successfully"))
     .catch((err) => console.log(err))
 
