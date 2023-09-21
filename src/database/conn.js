f6e5b7ee10313d1de812f4ecc0b6bf5880643043
Rajnish Tripathi <rajnish.tripathi@unthinkable.co>
2023-09-21 14:30:41 +0530

    env changed

--- src/database/conn.js
+++ src/database/conn.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const mongoose = require("mongoose")
 const DATABASE_URL = process.env.DATABASE_URL;
 
+
 mongoose.connect(DATABASE_URL)
     .then(() => console.log("database connected successfully"))
     .catch((err) => console.log(err))
