b9820e967a32d384c000dcc7d30626debdf9f14b
Rajnish Tripathi <rajnish.tripathi@unthinkable.co>
2023-09-21 16:03:29 +0530

    database url

--- src/database/conn.js
+++ src/database/conn.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 const mongoose = require("mongoose")
-const DATABASE_URl = process.env.DATABASE_URL;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 
-mongoose.connect(DATABASE_URl, {
+mongoose.connect(DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
