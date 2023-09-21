d4ab8161b1d14f14100396d22ea5218fd3cb7a4c
Rajnish Tripathi <rajnish.tripathi@unthinkable.co>
2023-09-21 14:24:09 +0530

    env added

--- src/database/conn.js
+++ src/database/conn.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const mongoose = require("mongoose")
+const DATABASE_URL = process.env.DATABASE_URL;
 
-mongoose.connect("mongodb://localhost:27017/e-commerce-bakend")
+mongoose.connect(DATABASE_URL)
     .then(() => console.log("database connected successfully"))
     .catch((err) => console.log(err))
 
